1f9c5dc0d9a27135efa063cc30739a9d80eb731e
Abdul Hay <mdabdulhay95@gmail.com>
2021-10-16 23:44:09 +0600

    First Init

---
+++ src/Components/About/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./about.css"
+
+const About = () => {
+    return (
+        <div className="a">
+            this is from About
+        </div>
+    )
+}
+
+export default About
---
+++ src/Components/Intro/Intro.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import "./intro.css"
+import Me from "../../Img/me1.png"
+
+const Intro = () => {
+    return (
+        <div className="i">
+            <div className="i-left">
+                <div className="i-left-wraper">
+                    <h2 className="i-intro">Hello! My name is </h2>
+                    <h1 className="i-name">Abdul Hay</h1>
+                    <div className="i-items">
+                        <div className="i-items-wraper">
+                            <div className="i-item">Web Developer</div>
+                            <div className="i-item">UX/UI Designer</div>
+                            <div className="i-item">Degital Marketer</div>
+                            <div className="i-item">Market Analyzer</div>
+                            <div className="i-item">Content Creator</div>
+                        </div>
+                    </div>
+                    <div className="i-desc">
+                        I design and develop services for customer of all sizes, spesializing in creating stylish, mordern websites, web services & online store.
+                    </div>
+                </div>
+            </div>
+            <div className="i-right">
+                <div className="i-bg"></div>
+                <img src={Me} alt="Me" className="i-img" />
+            </div>
+        </div>
+    )
+}
+
+export default Intro
