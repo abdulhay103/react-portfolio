426bbb9e5c3d2d9e92c4cb5b56c628add1313d01
Abdul Hay <mdabdulhay95@gmail.com>
2021-10-18 13:35:47 +0600

    About sec before award

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,19 +1,25 @@
 import React from 'react'
 import "./about.css"
-import UxUi from "../../Img/UX.jpg"
+import UxUi from "../../Img/pixel-Ui.jpg"
+import Award from "../../Img/award.jpg"
 
 const About = () => {
     return (
         <div className="a">
             <div className="a-left">
-                <div className="a-card bg">Card1</div>
+                <div className="a-card bg"></div>
                 <div className="a-card">
-                    <div className="a-img">
-                        <img src={UxUi} alt="UxUi" />
-                    </div>
+                    <img src={UxUi} alt="UxUi" className="a-img" />
                 </div>
             </div>
-            <div className="a-right">Right Side</div>
+            <div className="a-right">
+                <h1 className="a-title">About Me!</h1>
+                <p className="a-sub">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur facere nesciunt exercitationem quas. Quam, nam?</p>
+                <p className="a-desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, esse soluta? Ad id dolor autem rerum? Ea itaque voluptatem, repellat non voluptates quaerat eaque odio est eum reiciendis asperiores architecto neque deserunt veritatis, sint nemo commodi esse? Nisi quia similique aperiam, repellendus nobis nesciunt maiores asperiores esse molestiae ipsum officia?</p>
+                <div className="a-awards">
+                    <img src={Award} alt="Award" className="a-award-img" />
+                </div>
+            </div>
         </div>
     )
 }
