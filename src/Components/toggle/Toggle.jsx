f2fa73462fb96fc25e6d4cbb67cf3106289d1d2b
abdulhay103 <mdabdulhay95@gmail.comm>
2021-11-01 08:59:30 +0600

    Theme mode is running

--- src/Components/toggle/Toggle.jsx
+++ src/Components/toggle/Toggle.jsx
@@ -2,14 +2,13 @@ import React from 'react'
 import "./toggle.css"
 import Sun from "../../Img/sun.png"
 import Moon from "../../Img/moon.png"
-import Phone from "../../Img/phone.png"
 
 const Toggle = () => {
     return (
         <div className="t">
-            <img src={Sun} alt="" className="t-img" />
-            <img src={Moon} alt="" className="t-img" />
-            <img src={Phone} alt="" className="t-img" />
+            <img src={Sun} alt="" className="t-icon" />
+            <img src={Moon} alt="" className="t-icon" />
+            <div className="t-button"></div>
         </div>
     )
 }
